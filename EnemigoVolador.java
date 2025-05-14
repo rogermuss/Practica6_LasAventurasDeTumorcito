4ffc2fd0f2ad922d65aef2528597f213a7528c80
davidtq7 <juan.guzman40@uabc.edu.mx>
2025-05-14 01:27:57 -0700

    Update EnemigoVolador.java
    creacion

--- EnemigoVolador.java
+++ EnemigoVolador.java
@@ -3,8 +3,33 @@ package Practica_6;
 import java.awt.*;
 
 public class EnemigoVolador extends Enemigo {
+    private int velocidad = 2;  
+    private int limiteIzquierdo;  // Límite movimiento
+    private int limiteDerecho;    // Límite movimiento
+    private boolean moverHorizontal = false;  
+
     public EnemigoVolador(int x, int y, int ancho, int alto) {
         super(x, y, ancho, alto);
+    }
+
+    // Constructor con límites para el movimiento
+    public EnemigoVolador(int x, int y, int ancho, int alto, int limiteIzquierdo, int limiteDerecho) {
+        super(x, y, ancho, alto);
+        this.limiteIzquierdo = limiteIzquierdo;
+        this.limiteDerecho = limiteDerecho;
+        this.moverHorizontal = true;  
+    }
+
+    // Método para actualizar la posición del enemigo
+    public void actualizar() {
+        if (moverHorizontal) {
+            x += velocidad;
+
+            // Cambiar dirección si alcanza los límites
+            if (x <= limiteIzquierdo || x + ancho >= limiteDerecho) {
+                velocidad = -velocidad;
+            }
+        }
     }
 
     public void dibujar(Graphics g) {
